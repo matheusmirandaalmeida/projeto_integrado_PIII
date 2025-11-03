2bf89e3a78b3957f3a474543ad0157b3837bcd10
Matheus M Almeida <matheus.mma2015@gmail.com>
2025-11-02 22:13:04 -0300

    Gerando classes Cliente, Reserva e Funcionario.

--- estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Cliente.java
+++ estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Cliente.java
@@ -1,4 +1,36 @@
 package projeto_integrador.estacionamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cliente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String telefone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private LocalDateTime dataCadastro;
+
 }
--- estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Funcionario.java
+++ estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Funcionario.java
@@ -1,4 +1,32 @@
 package projeto_integrador.estacionamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "funcionario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String cargo;
 }
