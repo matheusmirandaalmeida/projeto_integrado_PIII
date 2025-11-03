219fbe03cc92c51e024afb7c95178ffbb27fdc88
Matheus M Almeida <matheus.mma2015@gmail.com>
2025-11-02 22:13:04 -0300

    Gerando classes Cliente, Reserva e Funcionario.

--- estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Reserva.java
+++ estacionamento/estacionamento/src/main/java/projeto_integrador/estacionamento/entity/Reserva.java
@@ -1,4 +1,39 @@
 package projeto_integrador.estacionamento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reserva")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @Column(nullable = false)
+    private LocalDateTime dataHorarioInicio;
+
+    @Column(nullable = false)
+    private LocalDateTime dataHorarioFim;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String vaga;
+
 }
